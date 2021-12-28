598f36ae36901f8e66782d844ae5b191e6f32f05
ShafeeMahmoud <shafee.mahmoud.alsafar@gmail.com>
2021-12-28 14:45:34 +0400

    Add authorName and title

--- lib/author_card.dart
+++ lib/author_card.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:fooderlich/circle_avatar.dart';
+import 'package:fooderlich/fooderlich_theme.dart';
 
 class AuthorCard extends StatelessWidget {
   const AuthorCard(
@@ -27,10 +28,23 @@ class AuthorCard extends StatelessWidget {
                 imageRadius: 28,
               ),
               const SizedBox(width: 8),
-
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    authorName,
+                    style: FooderlichTheme.lightTextTheme.headline2,
+                  ),
+                  Text(
+                    title,
+                    style: FooderlichTheme.lightTextTheme.headline3,
+                  ),
+                ],
+              ),
             ],
           ),
         ],
+        // TODO: Add icon button
       ),
     );
   }
